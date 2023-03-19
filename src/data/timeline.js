beae6ea85190895e96b25766e6c2460bb0bd0afe
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-03-19 15:56:06 -0400

    made style changes

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,35 +2,35 @@ export default [
 	{
 		year: 'Today',
 		title: 'Web Developer / Digital Operations Specialist',
-		duration: 'CURRENT',
+		duration: 'Current',
 		details:
 			'Working as a full-stack developer specializing in Shopify, WordPress and Click Funnels. I also work as a digital operations specialist helping troubleshoot any issues my coworkers may have with their computers and software and as well as work tech support for any live events and I can help anywhere from 50 to over 100 people in just a few hours to help resolve their technical issues.',
 	},
 	{
 		year: '2021',
 		title: 'Junior Web Developer',
-		duration: '1 YEAR',
+		duration: '1 Year',
 		details:
 			'I started working as a junior web developer at Citro Digital, a small web development and social media marketing company.  Here I really learnt a lot of working a real job and interacting with my teammates.  I also got to work on a lot of different projects and learn a lot of new technologies and services that would be used. I mainly worked with C#, .NET and the Kentico CMS to help build out websites and also manage content such as blog posts and downloadable content.',
 	},
 	{
 		year: '2019-2021',
 		title: 'Web Developer Internship',
-		duration: '2 YEARS',
+		duration: '2 Years',
 		details:
 			'I worked as a web developer intern at Vaswani Inc and this is where I really started to learn how to build websites and web applications.  I worked on the company website a lot using WordPress and also built web applications for inventory management and client management using React and NextJS along with Tailwind and Bootstrap.  I also got to work on a lot of different projects and learn a lot of new technologies and services that would be used.',
 	},
 	{
 		year: '2017-2021',
 		title: "Bachelor's Degree in Computer Science",
-		duration: '4 YEARS',
+		duration: '4 Years',
 		details:
 			'I attended college and got a bachelors degree in computer science.  I learned a lot not only about programming but also computer hardware, networking and security.  I also learned how to work in a team environment and how to communicate with others through group projects.',
 	},
 	{
 		year: '2014',
 		title: 'First Line of Code',
-		duration: 'THE BEGINNING',
+		duration: 'The Beginning',
 		details:
 			"I wrote my first line of code as a freshman in high school when I found out I didn't want to be in pharmacy school. I started with C++ and then moved on to Java in my senior year from a Java class in school.",
 	},
