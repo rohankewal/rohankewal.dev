db942a07ea2e9dd6689f15c4737ba330c6bcc7c5
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-04-14 20:51:47 -0400

    fixed a typo since I can't spell

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 export default function Intro() {
-	return (
-		<div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
-			<img src='/rohan.png' className='rounded-full w-36 h-36' />
-			<h1 className='pt-4 text-4xl md:text-6xl mb-1 md:mb-3 font-bold'>
-				I'm Rohan!
-			</h1>
-			<p className='text-base text-stone-600 md:text-xl mb-3 font-medium'>
-				Full-Stack Web Developer
-			</p>
-			<p className='text-stone-700 text-md max-w-xl mb-6 font-bold'>
-				I am a fullstack web developer from New Jersey. I work with WordPress,
-				Shopify and ReactJS. In my spare time, I love to play video games and
-				work out.
-			</p>
-			<br />
-			<p className='text-stone-700 text-md max-w-xl mb-6 font-bold'>
-				I will be documenting my journey as a developer and all other business
-				ventures on this blog and share thm with you. Here's to the future!
-			</p>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <img src="/rohan.png" className="rounded-full w-36 h-36" />
+      <h1 className="pt-4 text-4xl md:text-6xl mb-1 md:mb-3 font-bold">
+        I'm Rohan!
+      </h1>
+      <p className="text-base text-stone-600 md:text-xl mb-3 font-medium">
+        Full-Stack Web Developer
+      </p>
+      <p className="text-stone-700 text-md max-w-xl mb-6 font-bold">
+        I am a fullstack web developer from New Jersey. I work with WordPress,
+        Shopify and ReactJS. In my spare time, I love to play video games and
+        work out.
+      </p>
+      <br />
+      <p className="text-stone-700 text-md max-w-xl mb-6 font-bold">
+        I will be documenting my journey as a developer and all other business
+        ventures on this blog and share them with you. Here's to the future!
+      </p>
+    </div>
+  );
 }
