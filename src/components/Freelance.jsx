4378ccf9ff607367c71e3bfacc708ecc2cbc7ab6
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-03-05 22:45:48 -0500

    feat: added title to the freelance section

---
+++ src/components/Freelance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "./Title";
+import freelance from "../data/freelance";
+
+export default function Freelance() {
+    return(
+        <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+            <Title>Freelance Porjects</Title>
+        </div>
+    );
+}
