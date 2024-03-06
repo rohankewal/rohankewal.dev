832b9c268b3f026fd37149f5c892ce3193410ceb
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2024-03-05 21:25:14 -0500

    feat: working on adding freelance section to website

---
+++ src/data/freelance.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: "GP Automate",
+    logo: "",
+    service: "WordPress",
+  },
+  {
+    title: "Premier Medical Centre",
+    logo: "",
+    service: "SquareSpace",
+  }
+];
